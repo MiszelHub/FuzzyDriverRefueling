205b3895868280150ea070cd0e788f0f7f6388c2
Marcin <jamesjohny11@gmail.com>
2017-06-03 20:33:49 +0200

    Filled PetrolStation and Road classes

--- Fuzzy-Driver/src/main/java/symulation/data/PetrolStation.java
+++ Fuzzy-Driver/src/main/java/symulation/data/PetrolStation.java
@@ -3,5 +3,24 @@ package symulation.data;
 /**
  * Created by user on 03.06.2017.
  */
-public class PetrolStation {
+public class PetrolStation
+{
+    private int position = 0;
+    private float petrolPrice = 3.5f;
+
+    public PetrolStation(int position, float petrolPrice)
+    {
+        this.position = position;
+        this.petrolPrice = petrolPrice;
+    }
+
+    public int getPosition()
+    {
+        return position;
+    }
+
+    public float getPetrolPrice()
+    {
+        return petrolPrice;
+    }
 }
--- Fuzzy-Driver/src/main/java/symulation/data/Road.java
+++ Fuzzy-Driver/src/main/java/symulation/data/Road.java
@@ -3,5 +3,22 @@ package symulation.data;
 /**
  * Created by user on 03.06.2017.
  */
-public class Road {
+public class Road
+{
+    private int distance = 2000;
+
+    public Road()
+    {
+
+    }
+
+    public Road(int distance)
+    {
+        this.distance = distance;
+    }
+
+    public int getDistance()
+    {
+        return distance;
+    }
 }
