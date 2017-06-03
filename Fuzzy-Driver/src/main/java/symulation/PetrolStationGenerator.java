6585552b0e1670b21655e29896f0a26792140a50
andrzejczak195556 <nolanb.o.s@gmail.com>
2017-06-03 20:34:14 +0200

    car and petrolStation generatior Data

--- Fuzzy-Driver/src/main/java/symulation/PetrolStationGenerator.java
+++ Fuzzy-Driver/src/main/java/symulation/PetrolStationGenerator.java
@@ -1,7 +1,59 @@
 package symulation;
+
+import symulation.data.Road;
+
 /**
  * Created by user on 03.06.2017.
  */
 
 public class PetrolStationGenerator {
+    private Road road;
+    private int minimalDistanceBetweenStations = 50;
+    private int DistanceBetweenStations = 200;
+    private float minimalFuelPrice = 3.5f;
+    private float maximumFuelPrice = 4f;
+
+    public PetrolStationGenerator(Road road) {
+        this.road = road;
+    }
+
+    public Road getRoad() {
+        return road;
+    }
+
+    public void setRoad(Road road) {
+        this.road = road;
+    }
+
+    public int getMinimalDistanceBetweenStations() {
+        return minimalDistanceBetweenStations;
+    }
+
+    public void setMinimalDistanceBetweenStations(int minimalDistanceBetweenStations) {
+        this.minimalDistanceBetweenStations = minimalDistanceBetweenStations;
+    }
+
+    public int getDistanceBetweenStations() {
+        return DistanceBetweenStations;
+    }
+
+    public void setDistanceBetweenStations(int distanceBetweenStations) {
+        DistanceBetweenStations = distanceBetweenStations;
+    }
+
+    public float getMinimalFuelPrice() {
+        return minimalFuelPrice;
+    }
+
+    public void setMinimalFuelPrice(float minimalFuelPrice) {
+        this.minimalFuelPrice = minimalFuelPrice;
+    }
+
+    public float getMaximumFuelPrice() {
+        return maximumFuelPrice;
+    }
+
+    public void setMaximumFuelPrice(float maximumFuelPrice) {
+        this.maximumFuelPrice = maximumFuelPrice;
+    }
 }
--- Fuzzy-Driver/src/main/java/symulation/data/Car.java
+++ Fuzzy-Driver/src/main/java/symulation/data/Car.java
@@ -4,4 +4,45 @@ package symulation.data;
  * Created by user on 03.06.2017.
  */
 public class Car {
+
+    private int fuelCapacity;
+    private short fuelConsumptionPerHundredKilometers;
+    private Road road;
+    private int positionOnRoad=0;
+
+    public Car(Road road) {
+        this.road = road;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public short getFuelConsumptionPerHundredKilometers() {
+        return fuelConsumptionPerHundredKilometers;
+    }
+
+    public void setFuelConsumptionPerHundredKilometers(short fuelConsumptionPerHundredKilometers) {
+        this.fuelConsumptionPerHundredKilometers = fuelConsumptionPerHundredKilometers;
+    }
+
+    public Road getRoad() {
+        return road;
+    }
+
+    public void setRoad(Road road) {
+        this.road = road;
+    }
+
+    public int getPositionOnRoad() {
+        return positionOnRoad;
+    }
+
+    public void setPositionOnRoad(int positionOnRoad) {
+        this.positionOnRoad = positionOnRoad;
+    }
 }
